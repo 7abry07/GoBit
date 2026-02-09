6099edbc003071b8574edc806b0fb9c94642725a
Fabry <larosaf87@gmail.com>
2026-02-09 18:22:29 +0100

    created simple bencode encoder

--- internal/bencode/bencoder.go
+++ internal/bencode/bencoder.go
@@ -1,7 +1,49 @@
 package bencode
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func Encode(input string) string {
-	return fmt.Sprintf("encoded <%v>", input)
+func Encode(n BNode) string {
+	var result strings.Builder
+	if n.IsInt() {
+		result.WriteString(encodeInt(n._int))
+	} else if n.IsStr() {
+		result.WriteString(encodeStr(n._str))
+	} else if n.IsList() {
+		result.WriteString(encodeList(n._list))
+	} else if n.IsDict() {
+		result.WriteString((encodeDict(n._dict)))
+	}
+	return result.String()
+}
+
+func encodeInt(i BInt) string {
+	return fmt.Sprintf("i%de", i)
+}
+
+func encodeStr(s BStr) string {
+	return fmt.Sprintf("%d:%s", len(s), s)
+}
+
+func encodeList(l BList) string {
+	var result strings.Builder
+	result.WriteRune('l')
+	for _, val := range l {
+		result.WriteString(Encode(val))
+	}
+	result.WriteRune('e')
+	return result.String()
+}
+
+func encodeDict(d BDict) string {
+	var result strings.Builder
+	result.WriteRune('d')
+	for k, v := range d {
+		result.WriteString(encodeStr(BStr(k)))
+		result.WriteString(Encode(v))
+	}
+	result.WriteRune('e')
+	return result.String()
 }
